28ac31b12d54f45d4301f4872589fdb47df8a2ab
Rossana Chan <rossana1997a@gmail.com>
2025-01-27 01:43:05 +0800

    Add files via upload

---
+++ Codeforces/IAEPC Preliminary Contest (Codeforces Round 999, Div. 1 + Div. 2)/B.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+    
+
+
+}
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
